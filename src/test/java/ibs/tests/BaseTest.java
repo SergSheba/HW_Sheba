59a215b41032325b6a919e24690b1b034ebf9dcc
pinkp <shebaserg@gmail.com>
2024-04-18 23:00:20 +0300

    jenkins

--- src/test/java/ibs/tests/BaseTest.java
+++ src/test/java/ibs/tests/BaseTest.java
@@ -7,7 +7,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import ibs.pages.BasePage;
 import ibs.pages.ConfigPage;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
 
 
@@ -16,8 +20,30 @@ public abstract class BaseTest extends BasePage {
 
     @BeforeAll
     public static void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        // Проверяем, следует ли использовать Selenoid
+        String useSelenoid = System.getProperty("useSelenoid", "false");
+
+        if (Boolean.parseBoolean(useSelenoid)) {
+            // Конфигурация для Selenoid
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName("chrome");
+            capabilities.setVersion("100.0");
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", false);
+            try {
+                driver = new RemoteWebDriver(
+                        URI.create(ConfigPage.getProperty("selenoid.url")).toURL(),
+                        capabilities
+                );
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            // Локальный запуск через ChromeDriver
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -27,7 +53,10 @@ public abstract class BaseTest extends BasePage {
 
     @AfterAll
     public static void tearDown() {
-        driver.close();
-        driver.quit();
+        if (driver != null) {
+            driver.close();
+            driver.quit();
+        }
     }
 }
+
