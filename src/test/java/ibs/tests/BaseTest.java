67eb229715ea842cebda9b7bfd0e9e4ca4b585f0
pinkp <shebaserg@gmail.com>
2024-04-19 02:36:44 +0300

    update code

--- src/test/java/ibs/tests/BaseTest.java
+++ src/test/java/ibs/tests/BaseTest.java
@@ -32,7 +32,7 @@ public abstract class BaseTest extends BasePage {
             capabilities.setCapability("enableVideo", false);
             try {
                 driver = new RemoteWebDriver(
-                        URI.create(ConfigPage.getProperty("base_url")).toURL(),
+                        URI.create(ConfigPage.getProperty("selenoid.url")).toURL(),
                         capabilities
                 );
             } catch (MalformedURLException e) {
