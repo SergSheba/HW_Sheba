9189eb9342213fd9392a22e474826254bdffcd50
pinkp <shebaserg@gmail.com>
2024-04-19 01:42:15 +0300

    update code

--- src/test/java/ibs/tests/db_tests/BaseTestDb.java
+++ src/test/java/ibs/tests/db_tests/BaseTestDb.java
@@ -22,7 +22,7 @@ public class BaseTestDb {
     }
 
     protected static Connection getNewConnection() throws SQLException {
-        return DriverManager.getConnection(ConfigPage.getProperty("db_url"),
+        return DriverManager.getConnection(ConfigPage.getProperty("selenoid.url.db"),
                 ConfigPage.getProperty("db_login"), ConfigPage.getProperty("db_password"));
     }
 }
