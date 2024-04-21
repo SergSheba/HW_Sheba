f44abb916d7409d507397d4add7b0ce9d6efd5ce
pinkp <shebaserg@gmail.com>
2024-04-21 12:33:22 +0300

    update code

--- src/test/java/ibs/tests/BaseTest.java
+++ src/test/java/ibs/tests/BaseTest.java
@@ -14,50 +14,43 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.time.Duration;
+import java.util.Map;
 
 public abstract class BaseTest extends BasePage {
     protected static WebDriver driver;
 
     @BeforeAll
     public static void setUp() {
-        String browserType = System.getProperty("browser", "chrome");  // Дефолтное значение - Chrome
-        String useSelenoid = System.getProperty("useSelenoid", "false");  // По умолчанию не использовать Selenoid
-
-        if (Boolean.parseBoolean(useSelenoid)) {
+        if ("remote".equalsIgnoreCase(ConfigPage.getProperty("type_driver"))) {
             DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setBrowserName(browserType);
-            if (browserType.equals("firefox")) {
-                capabilities.setVersion("109.0");  // Указываем версию для Firefox
-            } else {
-                capabilities.setVersion("109.0");  // Указываем версию для Chrome
-            }
-            capabilities.setCapability("enableVNC", true);
-            capabilities.setCapability("enableVideo", false);
+            capabilities.setBrowserName(ConfigPage.getProperty("type_browser"));
+            capabilities.setVersion("109.0");
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", true,
+                    "enableVideo", false
+            ));
             try {
-                driver = new RemoteWebDriver(
-                        URI.create(ConfigPage.getProperty("selenoid.url")).toURL(),
-                        capabilities
-                );
+                driver = new RemoteWebDriver(URI.create(ConfigPage.getProperty("selenoid_url")).toURL(),
+                        capabilities, true);
             } catch (MalformedURLException e) {
                 throw new RuntimeException(e);
             }
+            driver.manage().window().maximize();
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            BasePage.setDriver(driver);
+            driver.get(ConfigPage.getProperty("remote_url"));
+
         } else {
-            if (browserType.equals("firefox")) {
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
-            } else {
-                WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
-            }
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            BasePage.setDriver(driver);
+            driver.get(ConfigPage.getProperty("base_url"));
         }
-
-        driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        BasePage.setDriver(driver);
-        driver.get(ConfigPage.getProperty("base_url"));
     }
-
     @AfterAll
     public static void tearDown() {
         if (driver != null) {
